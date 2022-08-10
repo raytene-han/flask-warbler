e3b4fd077ceda8f57dd7c1cb34733327d349d1a6
Phill Legnitto <palegnitto@gmail.com>
2022-08-10 14:14:26 -0700

    addd filter for homepage messages to show only messages from followed users

--- app.py
+++ app.py
@@ -357,10 +357,13 @@ def homepage():
     - anon users: no messages
     - logged in: 100 most recent messages of followed_users
     """
+    # breakpoint()
 
     if g.user:
+        following = [user.id for user in g.user.following]
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(following))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
